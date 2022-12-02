3388b0d8a242d8d4bf0cd8f845c6cc410220a320
Creaptis <73126498+Creaptis@users.noreply.github.com>
2022-12-02 11:38:29 +0100

    Add files via upload

--- Genhack_challenge_the_ginger_elephants/model.py
+++ Genhack_challenge_the_ginger_elephants/model.py
@@ -12,8 +12,11 @@
 ############################################################################
 
 import numpy as np
-import os
+#import os
 #import pandas as pd
+from sklearn.preprocessing import MinMaxScaler
+import tensorflow as tf
+
 
 # <!> DO NOT ADD ANY OTHER ARGUMENTS <!>
 def generative_model(noise):
@@ -27,13 +30,41 @@ def generative_model(noise):
     """
     # See below an example
     # ---------------------
-    latent_variable = noise[:, :50]  # use the first 10 dimensions of the noise
+    #latent_variable = noise[:, :10]  # use the first 10 dimensions of the noise
+    latent_variable = noise[:,:50]
+    
+    n_windows = 6330 # len(train_data) - seq_len = 9618 - 3288
+    batch_size = 128
+    
+    scaler = MinMaxScaler()
+    
+    # create 3288 * noise shape (3288,50)
+    def make_random_data():
+        while True:
+            yield latent_variable
 
+    random_series = iter(tf.data.Dataset
+                         .from_generator(make_random_data, output_types=tf.float32)
+                         .batch(batch_size)
+                         .repeat())
+    # model
+    new_model = tf.keras.models.load_model('/parameters/saved_model/my_model')
+    
+    # generated data
+    generated_data = []
+    for i in range(int(n_windows / batch_size)):
+        Z_ = next(random_series)
+        d = new_model(Z_)
+        generated_data.append(d)
+        
+    generated_data = np.array(np.vstack(generated_data))
+    generated_data = (scaler.inverse_transform(generated_data.reshape(-1, n_seq)).reshape(-1, seq_len, n_seq))
+        
     # load my parameters (of dimension 10 in this example). 
     # <!> be sure that they are stored in the parameters/ directory <!>
-    parameters = np.load(os.path.join("parameters", "param.npy"))
-
-    return np.maximum(0, latent_variable @ parameters)
+       # parameters = np.load(os.path.join("parameters", "param.npy"))
+    
+    return generated_data[0]
 
 
 
